a79e317a8ee60616ea3aa2ddd3dd0fe76cfedf4c
Franciscot93 <franciscot.p.93@gmail.com>
2023-05-22 15:08:18 -0300

    agrego codigo de localizacion de rostro

--- Reconocimiento_facial.py
+++ Reconocimiento_facial.py
@@ -12,9 +12,40 @@ foto_prueba= fr.load_image_file('fotoA.jpg')
 foto_control=cv2.cvtColor(foto_control, cv2.COLOR_BGR2RGB)
 foto_prueba=cv2.cvtColor(foto_prueba, cv2.COLOR_BGR2RGB)
 
+
+# rotacion y redimencion de imagenes
+
+foto_control1=cv2.rotate(cv2.resize(foto_control,(600,480)),cv2.ROTATE_90_COUNTERCLOCKWISE)
+foto_prueba1=cv2.rotate(cv2.resize(foto_prueba,(600,480)),cv2.ROTATE_90_COUNTERCLOCKWISE)
+
+#localizacion cara
+lugar_cara_A=fr.face_locations(foto_control1)[0]
+cara_codificada_A= fr.face_encodings(foto_control1)[0]
+
+lugar_cara_B=fr.face_locations(foto_prueba1)[0]
+cara_codificada_B= fr.face_encodings(foto_prueba1)[0]
+
+
+
+#mostrar rectangulo de control
+cv2.rectangle(foto_control1,
+              (lugar_cara_A[3],lugar_cara_A[0]),
+              (lugar_cara_A[1], lugar_cara_A[2])
+              ,(0,255,0),
+              2)
+
+cv2.rectangle(foto_prueba1,
+              (lugar_cara_B[3], lugar_cara_B[0]),
+              (lugar_cara_B[1], lugar_cara_B[2])
+              , (0, 255, 0),
+              2
+              )
+
+
+
 #mostrar imagenes
-cv2.imshow('Foto Control',foto_control)
-cv2.imshow('Foto Prueba', foto_prueba)
+cv2.imshow('Foto Control',foto_control1)
+cv2.imshow('Foto Prueba', foto_prueba1)
 
 #mantener programa abierto
 
