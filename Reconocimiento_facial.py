990ab862a2470e7c19b1e51b05508af8225cbdff
Franciscot93 <franciscot.p.93@gmail.com>
2023-05-22 12:49:56 -0300

    agregado de imagenes de control y prueba, asigancion de variables de control y prueba

--- Reconocimiento_facial.py
+++ Reconocimiento_facial.py
@@ -2,4 +2,20 @@ import cv2
 import face_recognition as fr
 
 
+# cargo imagenes para reconocimiento
 
+foto_control= fr.load_image_file('fotoB.jpg')
+foto_prueba= fr.load_image_file('fotoA.jpg')
+
+
+# paso imagenes a rgb
+foto_control=cv2.cvtColor(foto_control, cv2.COLOR_BGR2RGB)
+foto_prueba=cv2.cvtColor(foto_prueba, cv2.COLOR_BGR2RGB)
+
+#mostrar imagenes
+cv2.imshow('Foto Control',foto_control)
+cv2.imshow('Foto Prueba', foto_prueba)
+
+#mantener programa abierto
+
+cv2.waitKey(0)
